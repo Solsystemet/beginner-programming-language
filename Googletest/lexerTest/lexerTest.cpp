a2a1a1e2d8e42f083164fa79f7198afdec6c32c1
Jacob Mølholt Poulsen <43791907+lindejacob@users.noreply.github.com>
2025-04-23 14:52:49 +0200

    Started on lexer test, but have errors

---
+++ Googletest/lexerTest/lexerTest.cpp
@@ -0,0 +1,38 @@
+#include "pch.h"
+#include <gtest/gtest.h>
+#include "../../src/lexer/lexer.h"
+
+
+
+
+class LexerTest : public testing::Test {
+protected:
+    Lexer* lexer;
+    FILE* file;
+    std::vector<Token> tokens;
+
+    void SetUp() override {
+        file = fopen("test.txt", "r");
+        ASSERT_NE(file, nullptr) << "Failed to open file";
+        lexer = new Lexer(file);
+        tokens = lexer->Tokenize();
+    }
+
+    void TearDown() override {
+        fclose(file);
+        delete lexer;
+    }
+};
+
+TEST_F(LexerTest, TokenizeTest) {
+    ASSERT_EQ(tokens.size(), 10);
+    EXPECT_EQ(tokens[0].type, NEW_LINE);
+    EXPECT_EQ(tokens[1].type, TAB_INDENT);
+    EXPECT_EQ(tokens[2].type, DECIMAL);
+    EXPECT_EQ(tokens[3].type, IDENTIFIER);
+    EXPECT_EQ(tokens[4].type, COLON);
+    EXPECT_EQ(tokens[5].type, IF);
+    EXPECT_EQ(tokens[6].type, ELSE);
+    EXPECT_EQ(tokens[7].type, RETURN);
+    EXPECT_EQ(tokens[8].type, EOF);
+}
