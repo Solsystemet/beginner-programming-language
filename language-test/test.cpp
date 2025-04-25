3821fdfc18b1ce31561e66124a570731d363b2a2
MeisterRepsak <kasperschnejder@hotmail.com>
2025-04-25 13:18:38 +0200

    Fixed unit test

--- language-test/test.cpp
+++ language-test/test.cpp
@@ -1,18 +1,12 @@
 #include "pch.h"
-#include <gtest/gtest.h>
-#include "../../src/lexer/lexer.h"
-
-
-
-
+#include <filesystem>
 class LexerTest : public testing::Test {
-protected:
+public:
     Lexer* lexer;
     FILE* file;
     std::vector<Token> tokens;
-
     void SetUp() override {
-        file = fopen("test.txt", "r");
+        file = fopen( "C:\\.Dev\\beginner-programming-language\\language-test\\test.txt", "r");
         ASSERT_NE(file, nullptr) << "Failed to open file";
         lexer = new Lexer(file);
         tokens = lexer->Tokenize();
@@ -23,7 +17,6 @@ protected:
         delete lexer;
     }
 };
-
 TEST_F(LexerTest, TokenizeTest) {
     ASSERT_EQ(tokens.size(), 10);
     EXPECT_EQ(tokens[0].type, NEW_LINE);
@@ -34,5 +27,6 @@ TEST_F(LexerTest, TokenizeTest) {
     EXPECT_EQ(tokens[5].type, IF);
     EXPECT_EQ(tokens[6].type, ELSE);
     EXPECT_EQ(tokens[7].type, RETURN);
-    EXPECT_EQ(tokens[8].type, EOF);
+    EXPECT_EQ(tokens[8].type, TAB_DEDENT);
+    EXPECT_EQ(tokens[9].type, EOF);
 }
