97c1b8830b5d81d676baf75c54dfb518412cd04f
Jacob Mølholt Poulsen <43791907+lindejacob@users.noreply.github.com>
2025-05-12 19:46:45 +0200

    Added a bit of unittest

--- Test/ParserTest/parserTest.cpp
+++ Test/ParserTest/parserTest.cpp
@@ -12,13 +12,11 @@ public:
 
 protected:
     ParserMock parser;
-
-    void SetUp() override {}
-    void TearDown() override {}
 };
 
 
-TEST_F(ParserTest, TestParseArithmeticExpr) {
+//Aritmetic Expression Tests
+TEST_F(ParserTest, TestParseArithmeticExprMinus) {
 
     // Arrange
     parser.m_tokens.push_back({ DECIMAL, "2" });
@@ -32,5 +30,103 @@ TEST_F(ParserTest, TestParseArithmeticExpr) {
     ASSERT_NE(result, nullptr);
     ASSERT_TRUE(mpark::holds_alternative<node::NodeExprSubtract*>(result->var));
 }
+TEST_F(ParserTest, TestParseArithmeticExprPlus) {
+	// Arrange
+	parser.m_tokens.push_back({ DECIMAL, "2" });
+	parser.m_tokens.push_back({ PLUS });
+	parser.m_tokens.push_back({ DECIMAL, "2" });
+	// Act
+	node::NodeArithmeticExpr* result = parser.parse_arithmetic_expr();
+	// Assert
+	ASSERT_NE(result, nullptr);
+	ASSERT_TRUE(mpark::holds_alternative<node::NodeExprAdd*>(result->var));
+}
 
+TEST_F(ParserTest, TestParseTermMultiply) {
+	// Arrange
+	parser.m_tokens.push_back({ DECIMAL, "2" });
+	parser.m_tokens.push_back({ MULTIPLY });
+	parser.m_tokens.push_back({ DECIMAL, "2" });
+	// Act
+	node::NodeTerm* result = parser.parse_term();
+	// Assert
+	ASSERT_NE(result, nullptr);
+	ASSERT_TRUE(mpark::holds_alternative<node::NodeExprMult*>(result->var));
+}
 
+TEST_F(ParserTest, TestParseTermDivide) {
+	// Arrange
+	parser.m_tokens.push_back({ DECIMAL, "2" });
+	parser.m_tokens.push_back({ DIVIDE });
+	parser.m_tokens.push_back({ DECIMAL, "2" });
+	// Act
+	node::NodeTerm* result = parser.parse_term();
+	// Assert
+	ASSERT_NE(result, nullptr);
+	ASSERT_TRUE(mpark::holds_alternative<node::NodeExprDivide*>(result->var));
+}
+
+TEST_F(ParserTest, TestParseTermModulo) {
+	// Arrange
+	parser.m_tokens.push_back({ DECIMAL, "2" });
+	parser.m_tokens.push_back({ MODULO });
+	parser.m_tokens.push_back({ DECIMAL, "2" });
+	// Act
+	node::NodeTerm* result = parser.parse_term();
+	// Assert
+	ASSERT_NE(result, nullptr);
+	ASSERT_TRUE(mpark::holds_alternative<node::NodeExprModulo*>(result->var));
+}
+
+TEST_F(ParserTest, TestParseFactorDecimal) {
+	parser.m_tokens.push_back({ DECIMAL, "42" });
+
+	node::NodeFactor* result = parser.parse_factor();
+
+	ASSERT_NE(result, nullptr);
+	ASSERT_TRUE(mpark::holds_alternative<node::NodeFactorDecimal*>(result->var));
+}
+
+TEST_F(ParserTest, TestParseFactorIdentifier) {
+	parser.m_tokens.push_back({ IDENTIFIER, "x" });
+
+	node::NodeFactor* result = parser.parse_factor();
+
+	ASSERT_NE(result, nullptr);
+	ASSERT_TRUE(mpark::holds_alternative<node::NodeFactorIdentifier*>(result->var));
+}
+
+TEST_F(ParserTest, TestParseFactorParenthesizedExpr) {
+	parser.m_tokens.push_back({ OPEN_PARANTHESIS, "(" });
+	parser.m_tokens.push_back({ DECIMAL, "2" });
+	parser.m_tokens.push_back({ CLOSED_PARANTHESIS, ")" });
+
+	node::NodeFactor* result = parser.parse_factor();
+
+	ASSERT_NE(result, nullptr);
+	ASSERT_TRUE(mpark::holds_alternative<node::NodeArithmeticExpr*>(result->var));
+}
+TEST_F(ParserTest, TestParseStmtDeclaration) {
+	parser.m_tokens.push_back({ NUMBER, "number" });
+	parser.m_tokens.push_back({ IDENTIFIER, "x" });
+	parser.m_tokens.push_back({ EQUAL, "=" });
+	parser.m_tokens.push_back({ DECIMAL, "1" });
+	parser.m_tokens.push_back({ NEW_LINE, "\n" });
+
+	node::NodeStmt* result = parser.parse_stmt();
+
+	ASSERT_NE(result, nullptr);
+	ASSERT_TRUE(mpark::holds_alternative<node::NodeDecl*>(result->var));
+}
+
+TEST_F(ParserTest, TestParseStmtFunctionCall) {
+	parser.m_tokens.push_back({ IDENTIFIER, "foo" });
+	parser.m_tokens.push_back({ OPEN_PARANTHESIS, "(" });
+	parser.m_tokens.push_back({ CLOSED_PARANTHESIS, ")" });
+	parser.m_tokens.push_back({ NEW_LINE, "\n" });
+
+	node::NodeStmt* result = parser.parse_stmt();
+
+	ASSERT_NE(result, nullptr);
+	ASSERT_TRUE(mpark::holds_alternative<node::NodeFunctionCall*>(result->var));
+}
